2fca8ce0e4c0f10e05ea6b8f04bb2d90e8eb7411
gustavo-seidel-data <gustavomaykeldasilvaseidel@gmail.com>
2025-08-02 13:18:52 -0300

    feat(Model): add Model

--- neural_network/src/data_processing/controller_neural.py
+++ neural_network/src/data_processing/controller_neural.py
@@ -17,8 +17,18 @@ valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
 
 dataiter = iter(trainloader)
 imagens, etiquetas = next(dataiter)
-plt.imshow(imagens[0].numpy().squeeze(),cmap='gray_r');
 
-print(imagens[0].shape)
-print(etiquetas[0].shape)
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.linear1 = nn.Linear(28*28, 128)
+        self.linear2 = nn.Linear(128, 64)
+        self.linear3 = nn.Linear(64, 10)
+
+    def forward(self, X):
+        X = F.relu(self.linear1(X))
+        X = F.relu(self.linear2(X))
+        X = self.linear3(X)
+        return F.log_softmax(X, dim=1)
+
 
